66ff84e04d24e916ecda9284527fcd705fd91327
Srinivas <cnu.1439@gmail.com>
2017-08-23 11:27:21 +0530

    Adding disable form field.

---
+++ Block/System/Config/Form/Field/Disable.php
@@ -0,0 +1,13 @@
+<?php
+namespace Blockonomics\Merchant\Block\System\Config\Form\Field;
+
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Disable extends \Magento\Config\Block\System\Config\Form\Field
+{    
+  protected function _getElementHtml(AbstractElement $element)
+  {
+    $element->setData('readonly', 1);
+    return $element->getElementHtml();
+  }
+}
