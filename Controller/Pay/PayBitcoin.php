0fbcf1dc2e5d698fe77aa7765cd00c37f2b09b4c
Juha Siivikko <juha@siivikko.fi>
2017-11-04 17:28:54 +0200

    New controller Pay created, with PayBitcoin pagefactory

---
+++ Controller/Pay/PayBitcoin.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Blockonomics PlaceOrder controller
+ *
+ * @category    Blockonomics
+ * @package     Blockonomics_Merchant
+ * @author      Blockonomics
+ * @copyright   Blockonomics (https://blockonomics.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Blockonomics\Merchant\Controller\Pay;
+
+use \Magento\Framework\App\Action\Action;
+use \Magento\Framework\View\Result\PageFactory;
+use \Magento\Framework\View\Result\Page;
+use \Magento\Framework\App\Action\Context;
+use \Magento\Framework\Exception\LocalizedException;
+
+class PayBitcoin extends Action
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     *
+     * @codeCoverageIgnore
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct(
+            $context
+        );
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return Page
+     * @throws LocalizedException
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
