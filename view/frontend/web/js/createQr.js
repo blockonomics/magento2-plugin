89025c1256e518236c7238b56553c323e28f77b9
Juha Siivikko <juha@siivikko.fi>
2017-11-08 18:55:12 +0200

    Init commit of adding qr code generation needed js files

---
+++ view/frontend/web/js/createQr.js
@@ -0,0 +1,11 @@
+define([
+  "jquery",
+  "qrcode"
+], 
+function($, qrcode) {
+  "use strict";
+
+  // Here your custom code...
+  console.log(this.element, this.options);
+
+});
---
+++ view/frontend/web/js/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+
+  deps: [
+    "js/createQr"
+  ],
+
+  // Paths defines associations from library name (used to include the library,
+  // for example when using "define") and the library file path.
+  paths: {
+    'qrcode': 'js/qrcode.min',
+  }
+};
