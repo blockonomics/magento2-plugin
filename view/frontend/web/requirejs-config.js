f3c8263ac29d9a8c1c76ddbe5613944caab6aa00
Juha Siivikko <juha@siivikko.fi>
2017-11-11 18:00:56 +0200

    Added reconnecting libarary and dependecy

--- view/frontend/web/requirejs-config.js
+++ view/frontend/web/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
     map: {
         '*': {
             createQr: 'Blockonomics_Merchant/js/createQr',
-            qrcode: 'Blockonomics_Merchant/js/qrcode'
+            qrcode: 'Blockonomics_Merchant/js/qrcode',
+            reconnecting-websocket: 'Blockonomics_Merchant/js/reconnecting-websocket.min'
         }
     }
 };
