510279dc425e4ac9c8d5a5635bc9880cc1d7e4ce
Juha Siivikko <juha@siivikko.fi>
2017-11-12 18:16:56 +0200

    Transaction collection added

---
+++ Model/ResourceModel/Collection.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Blockonomics payment method model
+ *
+ * @category    Blockonomics
+ * @package     Blockonomics_Merchant
+ * @author      Blockonomics
+ * @copyright   Blockonomics (https://blockonomics.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Blockonomics\Merchant\Model\ResourceModel;
+
+use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection
+{
+    /**
+     * Remittance File Collection Constructor
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Blockonomics\Merchant\Model\BitcoinTransaction', 'Blockonomics\Merchant\Model\ResourceModel\BitcoinTransaction');
+    }
+}
