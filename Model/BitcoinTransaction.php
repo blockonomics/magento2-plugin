339397556df990f5a2b7b8310670e7f340868c28
Juha Siivikko <juha@siivikko.fi>
2017-11-12 17:20:43 +0200

    New model bitcoin transaction added

---
+++ Model/BitcoinTransaction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Blockonomics payment method model
+ *
+ * @category    Blockonomics
+ * @package     Blockonomics_Merchant
+ * @author      Blockonomics
+ * @copyright   Blockonomics (https://blockonomics.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Blockonomics\Merchant\Model;
+
+use \Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class Post extends AbstractDb
+{
+    /**
+     * Post Abstract Resource Constructor
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('toptal_blog_post', 'post_id');
+    }
+}
